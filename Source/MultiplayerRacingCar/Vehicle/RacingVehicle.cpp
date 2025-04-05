be191c055ebeed3a1194375173c113641fe19c32
RahmatM09 <rahmuh09@gmail.com>
2025-04-05 10:36:46 +0530

    Audio Components

--- Source/MultiplayerRacingCar/Vehicle/RacingVehicle.cpp
+++ Source/MultiplayerRacingCar/Vehicle/RacingVehicle.cpp
@@ -2,4 +2,43 @@
 
 
 #include "RacingVehicle.h"
+#include "Components/AudioComponent.h"
+#include "Net/UnrealNetwork.h"
 
+ARacingVehicle::ARacingVehicle()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
+	bAlwaysRelevant = true;
+
+	IdleAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("IdleAudioComponent"));
+	IdleAudioComponent->SetupAttachment(GetRootComponent());
+	IdleAudioComponent->bAutoActivate = false;
+
+	AccelerateAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("AccelerateAudioComponent"));
+	AccelerateAudioComponent->SetupAttachment(GetRootComponent());
+	AccelerateAudioComponent->bAutoActivate = false;
+
+	EngineAudioComponent = CreateDefaultSubobject<UAudioComponent>(TEXT("EngineAudioComponent"));
+	EngineAudioComponent->SetupAttachment(GetRootComponent());
+	EngineAudioComponent->bAutoActivate = false;
+	
+}
+
+void ARacingVehicle::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ARacingVehicle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void ARacingVehicle::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(ARacingVehicle, IdleSound);
+	DOREPLIFETIME(ARacingVehicle, AccelerateSound);
+	DOREPLIFETIME(ARacingVehicle, EngineSound);
+}
--- Source/MultiplayerRacingCar/Vehicle/RacingVehicle.h
+++ Source/MultiplayerRacingCar/Vehicle/RacingVehicle.h
@@ -7,7 +7,8 @@
 #include "RacingVehicle.generated.h"
 
 class UGameEndAnnouncement;
-
+class UAudioComponent;
+class USoundBase;
 /**
  * 
  */
@@ -16,8 +17,36 @@ class MULTIPLAYERRACINGCAR_API ARacingVehicle : public AWheeledVehiclePawn
 {
 	GENERATED_BODY()
 
-
 public:
+	ARacingVehicle();
+	
 	UPROPERTY(EditAnywhere, Category = "WinnerAnnouncement")
 	TObjectPtr<UGameEndAnnouncement> GameAnnouncement;
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+	
+private:
+	// Audio Components
+	UPROPERTY(VisibleAnywhere, Category = "Audio Components")
+	TObjectPtr<UAudioComponent> IdleAudioComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Audio Components")
+	TObjectPtr<UAudioComponent> AccelerateAudioComponent;
+
+	UPROPERTY(VisibleAnywhere, Category = "Audio Components")
+	TObjectPtr<UAudioComponent> EngineAudioComponent;
+
+	// Audio Variable
+	UPROPERTY(Replicated, EditAnywhere, Category = "Audio")
+	TObjectPtr<USoundBase> IdleSound;
+
+	UPROPERTY(Replicated, EditAnywhere, Category = "Audio")
+	TObjectPtr<USoundBase> AccelerateSound;
+
+	UPROPERTY(Replicated, EditAnywhere, Category = "Audio")
+	TObjectPtr<USoundBase> EngineSound;
+	
 };
